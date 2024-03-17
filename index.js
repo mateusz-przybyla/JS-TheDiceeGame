eb279532a68f26f60f5dd5f3972efd3e00aefa07
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-03-17 14:07:55 +0100

    refreshMe() function

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var randomNumber1 = 0;
 
+window.onload = refreshMe;
+
 function getRandomNumber() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-randomNumber1 = getRandomNumber();
-console.log(randomNumber1);
+function refreshMe() {
+  randomNumber1 = getRandomNumber();
+
+  document
+    .querySelector(".img1")
+    .setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+}
